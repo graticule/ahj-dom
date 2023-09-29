e67a0f4983b2998ef63074b064fb4517dae40663
Dan Kozlov <kozlov-da@yandex.ru>
2023-09-29 10:37:40 +0800

    Add tests for game.js

--- src/js/game/game.test.js
+++ src/js/game/game.test.js
@@ -4,9 +4,26 @@
 
 import Game from "./game";
 
+const element = document.createElement("div");
+const game = new Game(element);
+
 test("check number of cells", () => {
-  const element = document.createElement("div");
-  const game = new Game(element);
   expect(element.querySelectorAll(".game__row").length).toBe(4);
   expect(element.querySelectorAll(".game__cell").length).toBe(16);
 });
+
+test("change position test", () => {
+  expect(element.querySelectorAll('.game__cell_active').length).toBe(0);
+  for(let i=0; i < 10; i++){
+    const prev = element.querySelector('.game__cell_active');  
+    game.changePosition();
+    const next = element.querySelector('.game__cell_active');
+    if(prev){
+      expect( prev.isEqualNode(next)).toBeFalsy();
+    } else {
+      expect(prev).toBeNull();
+      expect(next).not.toBeNull();
+    }
+    expect(element.querySelectorAll('.game__cell_active').length).toBe(1);
+  }
+})
